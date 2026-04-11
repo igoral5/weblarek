fc8aac4effe9a94f342e6d15f64215f71e11edab
Igor Alimov <ilalimov@mts.ru>
2026-04-11 20:39:59 +0300

    Add comments

--- src/main.ts
+++ src/main.ts
@@ -37,6 +37,9 @@ const header = new Header(ensureElement(".header"), events);
 
 const buyer = new Buyer(events);
 
+/**
+ * Реализует связи между моделями и представлениями с помощью брокера событий
+ */
 class Presenter {
   protected basket: Basket | undefined;
   protected order: Order | undefined;
@@ -55,13 +58,17 @@ class Presenter {
     this.configure();
   }
 
+  /**
+   * Задает обработчики событий
+   */
   protected configure() {
+    // Событие загрузки каталога
     this.events.on("catalog:load", () => {
       this.client.getProducts().then((data) => {
         this.catalog.setProcucts(data);
       });
     });
-
+    // Событие изменения каталога, приводит к его показу
     this.events.on("catalog:change", () => {
       this.galllery.render({
         catalog: this.catalog.getProducts().map((product) => {
@@ -78,11 +85,11 @@ class Presenter {
         }),
       });
     });
-
+    // Выбор пользователем продукта для просмотра
     this.events.on("catalog:select", (product) => {
       this.catalog.setSelected(product as IProduct);
     });
-
+    // Устанавливаем продукт в качестве выбранного, приводит к его показу в модальном окне
     this.events.on("catalog:selected", () => {
       const product = this.catalog.getSelected() as IProduct;
       const selected = this.cart.isExist(product.id);
@@ -105,17 +112,17 @@ class Presenter {
       });
       this.modal.show = true;
     });
-
+    // Закрытие модального окна
     this.events.on("modal:close", () => {
       this.modal.show = false;
     });
-
+    // Корзина пользователя изменена, приводит к изменению заголовка
     this.events.on("basket:change", () => {
       this.header.render({
         counter: this.cart.count(),
       });
     });
-
+    // Открытие корзины пользователя
     this.events.on("basket:open", () => {
       this.basket = new Basket(cloneTemplate("#basket"), this.events);
       this.modal.render({
@@ -125,7 +132,7 @@ class Presenter {
       this.events.on("basket:change", this.renderBasket);
       this.events.on("modal:close", this.closeBasket);
     });
-
+    // Открытие первой части заказа, ввод способа оплаты и адреса
     this.events.on("order:open", () => {
       this.order = new Order(cloneTemplate("#order"), this.events);
 
@@ -136,15 +143,15 @@ class Presenter {
       this.events.on("buyer:change", this.renderOrder);
       this.events.on("modal:close", this.closeOrder);
     });
-
+    // Ввод способа оплаты
     this.events.on("order:payment", (data: { payment: TPayment }) => {
       this.buyer.setPayment(data.payment);
     });
-
+    // Ввод адреса
     this.events.on("order:address", (data: { address: string }) => {
       this.buyer.setAddress(data.address);
     });
-
+    // Закрытие первой части ввода заказа и переход на вторую часть, где производиться ввод email и телефона
     this.events.on("order:close", () => {
       this.contacts = new Contacts(cloneTemplate("#contacts"), this.events);
       this.modal.render({
@@ -154,15 +161,15 @@ class Presenter {
       this.events.on("modal:close", this.closeContacts);
       this.modal.show = true;
     });
-
+    // Ввод email
     this.events.on("contacts:email", (data: { email: string }) => {
       this.buyer.setEmail(data.email);
     });
-
+    // Ввод телефона
     this.events.on("contacts:phone", (data: { phone: string }) => {
       this.buyer.setPhone(data.phone);
     });
-
+    // Завершение ввода заказа и отправка его на back, показ заключительного модального окна
     this.events.on("contacts:close", () => {
       const buyer = this.buyer.getBuyer();
       const order = {
@@ -182,6 +189,10 @@ class Presenter {
     });
   }
 
+  /**
+   * Показывает корзину пользователя, представлено в виде свойства, для возможности удаления этого обработчика из брокера событий
+   * @returns Разметка
+   */
   protected renderBasket = () => {
     return this.basket?.render({
       cost: this.cart.cost(),
@@ -200,12 +211,19 @@ class Presenter {
     });
   };
 
+  /**
+   * Закрывает корзину пользователя, удаляет связанные с ней обработчики
+   */
   protected closeBasket = () => {
     this.basket = undefined;
     this.events.off("basket:change", this.renderBasket);
     this.events.off("modal:close", this.closeBasket);
   };
 
+  /**
+   * Показывает первую часть заказа с вводом способа оплаты и адресом
+   * @returns Разметка
+   */
   protected renderOrder = () => {
     const buyer = this.buyer.getBuyer();
     const { email, phone, ...error } = this.buyer.validate();
@@ -217,12 +235,19 @@ class Presenter {
     });
   };
 
+  /**
+   * Закрывает первую часть ввода заказа и удаляет связанные с ней обработчики
+   */
   protected closeOrder = () => {
     this.order = undefined;
     this.events.off("buyer:change", this.renderOrder);
     this.events.off("modal:close", this.closeOrder);
   };
 
+  /**
+   * Показывает вторую часть ввода заказа, ввод email и телефона
+   * @returns Разметка
+   */
   protected renderContacts = () => {
     const buyer = this.buyer.getBuyer();
     const { payment, address, ...error } = this.buyer.validate();
@@ -234,12 +259,18 @@ class Presenter {
     });
   };
 
+  /**
+   * Закрывает вторую часть ввода заказа и удаляет связанные с ним обработчики
+   */
   protected closeContacts = () => {
     this.contacts = undefined;
     this.events.off("buyer:change", this.renderContacts);
     this.events.off("modal:close", this.closeContacts);
   };
 
+  /**
+   * Закрывает завершающее модальное окно, об успешном заказе
+   */
   protected closeSuccess = () => {
     this.cart.clear();
     this.buyer.setBuyer({
@@ -251,6 +282,9 @@ class Presenter {
     this.events.off("modal:close", this.closeSuccess);
   };
 
+  /**
+   * Инициирует начальную загрузку каталога
+   */
   start() {
     this.events.emit("catalog:load");
   }
